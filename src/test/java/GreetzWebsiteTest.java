bbc12053372b89af611d6d07f7fa6fa1528bb0d8
Hayk Melikyan <hayk_melikyan@epam.com>
2021-03-24 19:49:52 +0400

    Add testNG Before-s and After-s Add Surefire plugin Make corrections

--- src/test/java/GreetzWebsiteTest.java
+++ src/test/java/GreetzWebsiteTest.java
@@ -3,29 +3,63 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class GreetzWebsiteTest {
 
-    private  WebDriver driver;
+    private WebDriver driver;
 
-    @Test
+    @BeforeMethod
+    public void loginToGreetz() {
+        // Set driver property, create browser instance
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        // Go to the greetz.nl login page
+        driver.get("https://www.greetz.nl/auth/login");
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        // Close the popup window
+        driver.findElement(By.xpath("//span[text() = 'Akkoord']")).click();
+
+        // Fill the fields and log in
+        WebElement inputFields = driver.findElement(By.className("inputfields"));
+        inputFields.findElement(By.name("email")).sendKeys("vander@vaart.com");
+        inputFields.findElement(By.name("password")).sendKeys("holand5654");
+        driver.findElement(By.id("login-cta")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @Test(priority = 0)
     public void favoritesPageTest() throws InterruptedException {
-        // Login to greetz.nl
-        login();
-
         // Go to baloons' page
         driver.get("https://www.greetz.nl/ballonnen/denken-aan");
         Thread.sleep(2000);
 
         // Get first product's name and price
         WebElement productItem = driver.findElement(By.cssSelector("div.b-products-grid__item:nth-child(1)"));
-        String ExpectedProductName = productItem.findElement(By.className("b-products-grid__item-title")).getText();
-        String ExpectedProductPrice = "€ " + productItem.findElement(By.className("b-products-grid__item-price")).getText();
+        String expectedProductName = productItem.findElement(By.className("b-products-grid__item-title")).getText();
+        String expectedProductPrice = "€ " + productItem.findElement(By.className("b-products-grid__item-price")).getText();
 
         // Add product to favorites
         productItem.findElement(By.className("b-products-grid__item-action")).click();
+        Thread.sleep(500);
 
         // Go to favorites' page
         driver.findElement(By.cssSelector("a[class = 'page-header__navigation-item item-user']")).click();
@@ -40,24 +74,22 @@ public class GreetzWebsiteTest {
         WebElement sidebar = driver.findElement(By.className("page-detail__sidebar-container"));
 
         // Check name and price
+        String actualProductName = sidebar.findElement(By.className("giftdetails--title")).getText();
+        String actualProductPrice = sidebar.findElement(By.className("price-block")).getText();
+
         SoftAssert assertion = new SoftAssert();
-        assertion.assertEquals(sidebar.findElement(By.tagName("h1")).getText(), ExpectedProductName,
+        assertion.assertEquals(actualProductName, expectedProductName,
                 "Product with such name hadn't been find in the favorites");
-        Assert.assertEquals(sidebar.findElement(By.className("price-block")).getText(), ExpectedProductPrice,
+        Assert.assertEquals(actualProductPrice, expectedProductPrice,
                 "Product with such price hadn't been find in the favorites");
 
         // Remove the product from favorites, so the test can be repeated on this product
         driver.findElement(By.className("productdetails-favorite")).click();
-
-        // Close the browser
-        driver.quit();
+        Thread.sleep(500);
     }
 
-    @Test
+    @Test(priority = 1)
     public void priceAmountTest() throws InterruptedException {
-        // Login to greetz.nl
-        login();
-
         // Go to cards' page
         driver.get("https://www.greetz.nl/kaarten/denken-aan");
         Thread.sleep(2000);
@@ -74,9 +106,12 @@ public class GreetzWebsiteTest {
 
         // Obtain prices
         WebElement priceBlock = driver.findElement(By.className("page-detail__price"));
-        String singlePrice = priceBlock.findElement(By.className("price-normal")).getText().substring(2);
+
+        String singlePrice = priceBlock.findElement(By.className("price-normal")).getText();
+        singlePrice = singlePrice.replaceAll("[^(\\d|\\,)]", "");
+
         String totalPrice = priceBlock.findElement(By.className("price-total")).getText();
-        totalPrice = totalPrice.substring(9, totalPrice.indexOf('e') - 1);
+        totalPrice = totalPrice.replaceAll("[^(\\d|\\,)]", "");
 
         // Replace , with .
         singlePrice = singlePrice.replace(',', '.');
@@ -88,28 +123,5 @@ public class GreetzWebsiteTest {
 
         //Compare prices
         Assert.assertEquals(totalPriceActual, totalPriceExpected, "Total price is wrong");
-
-        // Close the browser
-        driver.quit();
-    }
-
-    private void login() throws InterruptedException {
-        // Set driver property, create browser instance
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
-
-        // Go to the greetz.nl login page
-        driver.get("https://www.greetz.nl/auth/login");
-        Thread.sleep(2000);
-
-        // Close the popup window
-        driver.findElement(By.xpath("//span[text() = 'Akkoord']")).click();
-
-        // Fill the fields and log in
-        WebElement inputFields = driver.findElement(By.className("inputfields"));
-        inputFields.findElement(By.name("email")).sendKeys("vander@vaart.com");
-        inputFields.findElement(By.name("password")).sendKeys("holand5654");
-        driver.findElement(By.id("login-cta")).click();
-        Thread.sleep(2000);
     }
 }
