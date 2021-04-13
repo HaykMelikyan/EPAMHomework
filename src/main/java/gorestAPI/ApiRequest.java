4c02d844c768e9369184d251ea910f6b7e050ba1
Hayk Melikyan <Hayk_Melikyan@epam.com>
2021-04-13 18:08:13 +0400

    Add request and response specifications.

--- src/main/java/gorestAPI/ApiRequest.java
+++ src/main/java/gorestAPI/ApiRequest.java
@@ -1,43 +1,67 @@
 package gorestAPI;
 
+import core.PropertyReader;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
-import io.restassured.http.Header;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import static org.hamcrest.Matchers.is;
 
 public class ApiRequest {
-    private Header header = new Header("Authorization", "Bearer 53b517c08020390a239e02b19714d9e6665715878869e69f811d3e81e8a0029a");
-    private int postId;
-    private String endPoint;
+    private final PropertyReader propReader = new PropertyReader("src/main/resources/ApiRequestProp.properties");
 
-    public ApiRequest(String endPoint) {
-        RestAssured.baseURI = "https://gorest.co.in/";
-        RestAssured.basePath = "public-api/";
-        this.endPoint = endPoint;
+    public ApiRequest() {
+        RestAssured.baseURI = propReader.getProperty("BASE_URI");
+        RestAssured.basePath = propReader.getProperty("BASE_PATH");
     }
 
-    public int post(Object requestBody) {
-        Response postResponse = RestAssured
+    public Response post(String endPoint, Object requestBody) {
+        return RestAssured
                 .given()
-                .header(header)
-                .contentType(ContentType.JSON)
+                .spec(requestSpec())
                 .body(requestBody)
                 .post(endPoint)
-                .thenReturn();
-        postId = postResponse.jsonPath().getInt("data.id");
-        return postId;
+                .then()
+                .spec(responseSpec())
+                .extract()
+                .response();
     }
 
-    public Response get() {
+    public Response get(String endPoint) {
         return RestAssured
-                .get(endPoint + postId)
-                .thenReturn();
+                .get(endPoint)
+                .then()
+                .spec(responseSpec())
+                .extract()
+                .response();
     }
 
-    public void delete() {
-        RestAssured
+    public Response delete(String endPoint) {
+        return RestAssured
                 .given()
-                .header(header)
-                .delete(endPoint + postId);
+                .spec(requestSpec())
+                .delete(endPoint)
+                .then()
+                .spec(responseSpec())
+                .extract()
+                .response();
+    }
+
+    private RequestSpecification requestSpec() {
+        return new RequestSpecBuilder()
+                .addHeader("Authorization", propReader.getProperty("AUTHTORIZATION_TOKEN"))
+                .setContentType(ContentType.JSON)
+                .build();
+    }
+
+    private ResponseSpecification responseSpec() {
+        return new ResponseSpecBuilder()
+                .expectContentType(ContentType.JSON)
+                .expectStatusCode(is(200))
+                .build();
     }
 }
--- src/test/java/GoRestApiTest.java
+++ src/test/java/GoRestApiTest.java
@@ -1,40 +1,45 @@
 import gorestAPI.ApiRequest;
 import gorestAPI.User;
 import io.restassured.response.Response;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 public class GoRestApiTest {
-    private ApiRequest apiRequest;
-
-    @BeforeClass
-    public void setBase() {
-        apiRequest = new ApiRequest("users/");
-    }
-
-    @AfterClass
-    public void deleteUser() {
-        apiRequest.delete();
-    }
+    private User user;
 
     @Test
     public void goRestPostUserTest() {
-        User user = User.getRandomUser();
+        ApiRequest apiRequest = new ApiRequest();
+        String endPoint = "users/";
+        user = User.getRandomUser();
 
         // Post a user and obtain id
-        apiRequest.post(user);
+        Response postResponse = apiRequest.post(endPoint, user);
+        checkResponse(postResponse);
+        String userId = getField(postResponse, "data.id");
 
         // Get user by id
-        Response getResponse = apiRequest.get();
+        Response getResponse = apiRequest.get(endPoint + userId);
+        checkResponse(getResponse);
 
-        // Assertions
+        // Delete user by id
+        Response deleteResponse = apiRequest.delete(endPoint + userId);
+        checkDelete(deleteResponse);
+    }
+
+    private void checkResponse(Response response) {
         SoftAssert assertion = new SoftAssert();
-        assertion.assertEquals(getField(getResponse, "data.name"), user.getName(), "Name doesn't match.");
-        assertion.assertEquals(getField(getResponse, "data.email"), user.getEmail(), "Email doesn't match.");
-        assertion.assertEquals(getField(getResponse, "data.gender"), user.getGender(), "Gender doesn't match.");
-        assertion.assertEquals(getField(getResponse, "data.status"), user.getStatus(), "Status doesn't match.");
+        assertion.assertEquals(getField(response, "data.name"), user.getName(), "Name doesn't match.");
+        assertion.assertEquals(getField(response, "data.email"), user.getEmail(), "Email doesn't match.");
+        assertion.assertEquals(getField(response, "data.gender"), user.getGender(), "Gender doesn't match.");
+        assertion.assertEquals(getField(response, "data.status"), user.getStatus(), "Status doesn't match.");
+        assertion.assertAll();
+    }
+
+    private void checkDelete(Response response) {
+        SoftAssert assertion = new SoftAssert();
+        assertion.assertEquals(getField(response, "code"), "204", "Delete status code is not 204.");
+        assertion.assertEquals(getField(response, "data"), null, "Deleted data isn't null.");
         assertion.assertAll();
     }
 
